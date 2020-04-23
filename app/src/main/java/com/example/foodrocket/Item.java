ed44df407cfd17f2ba43d905d84f17f01d970597
emaz <emaz111@hotmail.com>
2020-04-24 00:25:35 +0100

    Added menu item card view

---
+++ app/src/main/java/com/example/foodrocket/Item.java
@@ -0,0 +1,14 @@
+package com.example.foodrocket;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Item extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+}
--- app/src/main/java/com/example/foodrocket/MenuFragment.java
+++ app/src/main/java/com/example/foodrocket/MenuFragment.java
@@ -1,9 +1,12 @@
 package com.example.foodrocket;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +17,8 @@ import android.view.ViewGroup;
  */
 public class MenuFragment extends Fragment {
 
+    CardView itemCard;
+
     public MenuFragment() {
         // Required empty public constructor
     }
@@ -23,6 +28,23 @@ public class MenuFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+        //itemCard = (CardView) getView().findViewById(R.id.pizza1);
+
+        //itemCard.setOnClickListener(this);
+
         return inflater.inflate(R.layout.fragment_menu, container, false);
+
     }
+
+//    @Override
+//    public void onClick(View v) {
+//        switch (v.getId()){
+//            case R.id.pizza1 :
+//                Intent intent = new Intent(getContextApplication(), Login.class);
+//                startActivity(intent);
+//                break;
+//            default: break;
+//        }
+//    }
 }
