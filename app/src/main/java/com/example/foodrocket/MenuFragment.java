f6fa77f03c7a30804f6b7cf0cf43e54482597bc4
emaz <emaz111@hotmail.com>
2020-04-30 01:26:04 +0100

    Added item adapter with recycler

--- app/src/main/java/com/example/foodrocket/MenuFragment.java
+++ app/src/main/java/com/example/foodrocket/MenuFragment.java
@@ -2,17 +2,28 @@ package com.example.foodrocket;
 
 import android.animation.ArgbEvaluator;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,14 +33,23 @@ import java.util.List;
  */
 public class MenuFragment extends Fragment implements View.OnClickListener{
 
-    CardView itemCard;
 
-    RecyclerView recyclerView;
-    ViewPager viewPager;
-    Adapter adapter;
-    List<PopularItemModel> models;
-    Integer[] colors = null;
-    ArgbEvaluator argbEvaluator = new ArgbEvaluator();
+//    RecyclerView recyclerView;
+//    ViewPager viewPager;
+//    Adapter adapter;
+//    List<PopularItemModel> models;
+//    ArgbEvaluator argbEvaluator = new ArgbEvaluator();
+
+    CardView itemCard;
+    RecyclerView dataList;
+    List<String> titles;
+    List<Integer> images;
+    List<String> desc;
+    ItemAdapter adapter;
+    ArrayList<MenuItem> items = new ArrayList<>();
+
+    final String SHARED_PREFS = "sharedPrefs";
+    final String USER_TOKEN = "user_token";
 
     public MenuFragment() {
         // Required empty public constructor
@@ -47,25 +67,41 @@ public class MenuFragment extends Fragment implements View.OnClickListener{
 
         //itemCard.setOnClickListener(this);
 
-        models = new ArrayList<>();
-        models.add(new PopularItemModel(R.drawable.pizza, "Pizza", "Fresh Backed Pizza"));
-        models.add(new PopularItemModel(R.drawable.pizza, "Pizza", "Fresh Backed Pizza"));
-        models.add(new PopularItemModel(R.drawable.pizza, "Pizza", "Fresh Backed Pizza"));
-        models.add(new PopularItemModel(R.drawable.pizza, "Pizza", "Fresh Backed Pizza"));
+//        models = new ArrayList<>();
+//        models.add(new PopularItemModel(R.drawable.pizza, "Pizza", "Fresh Backed Pizza"));
+//        models.add(new PopularItemModel(R.drawable.pizza, "Pizza", "Fresh Backed Pizza"));
+//        models.add(new PopularItemModel(R.drawable.pizza, "Pizza", "Fresh Backed Pizza"));
+//        models.add(new PopularItemModel(R.drawable.pizza, "Pizza", "Fresh Backed Pizza"));
+//
+//        adapter = new Adapter(models, (Context) getActivity());
+//
+//        viewPager = view.findViewById(R.id.viewPager);
+//        viewPager.setAdapter(adapter);
+//        viewPager.setPadding(200, 0, 200, 0);
+//
 
-        adapter = new Adapter(models, (Context) getActivity());
+        dataList = view.findViewById(R.id.recycler_menu);
 
-        viewPager = view.findViewById(R.id.viewPager);
-        viewPager.setAdapter(adapter);
-        viewPager.setPadding(200, 0, 200, 0);
+        titles = new ArrayList<>();
+        images = new ArrayList<>();
+        desc = new ArrayList<>();
 
-        Integer[] colors_temp = {
-                getResources().getColor(R.color.UI_LightBlue),
-                getResources().getColor(R.color.UI_LightBlue),
-                getResources().getColor(R.color.UI_DarkBlue),
-                getResources().getColor(R.color.UI_Red)
-        };
 
+        titles.add("First Item");
+        titles.add("Second Item");
+
+        images.add(R.drawable.pizza);
+        images.add(R.drawable.pizza);
+
+        desc.add("ADFDSFDSF");
+        desc.add("ADFDSFDSF");
+
+
+        adapter = new ItemAdapter((Context) getActivity(),titles,images, desc);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager((Context) getActivity(),2,GridLayoutManager.VERTICAL,false);
+        dataList.setLayoutManager(gridLayoutManager);
+        dataList.setAdapter(adapter);
 
 
         return view;
@@ -75,15 +111,35 @@ public class MenuFragment extends Fragment implements View.OnClickListener{
     @Override
     public void onStart() {
         super.onStart();
-        //defaultGetItems();
+        defaultGetItems();
     }
 
-    // Recycler view fetch items
-//    private void defaultGetItems() {
-//
+    private void defaultGetItems() {
+        ApiMenuItemRequest items_api = new ApiMenuItemRequest();
+        SharedPreferences sharedPreferences = this.getActivity().getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        String token = sharedPreferences.getString(USER_TOKEN, "");
+        JsonParser parser = new JsonParser();
+
+        try {
+            String response = items_api.execute(token).get();
+            JsonObject json_response = parser.parse(response).getAsJsonObject();
+            if (json_response.has("success")) {
+                Gson gson = new Gson();
+                Type type = new TypeToken<ArrayList<MenuItem>>() {}.getType();
+                items = gson.fromJson(json_response.getAsJsonArray("success"), type);
+            } else if (json_response.has("error")) { // Invalid credentials
+                String error = json_response.get("error").toString();
+                Toast.makeText(this.getContext(), error,Toast.LENGTH_LONG).show();
+            } else { // Some other error :(
+                Toast.makeText(this.getContext(), response, Toast.LENGTH_LONG).show();
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 //        recyclerView.setAdapter(adapter);
 //        adapter.startListening();
-//    }
+    }
 
     @Override
     public void onClick(View v) {
@@ -94,5 +150,18 @@ public class MenuFragment extends Fragment implements View.OnClickListener{
 //                break;
 //            default: break;
 //        }
+    }
+
+    class ApiMenuItemRequest extends AsyncTask<String,Void,String> {
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            GetRequestHandler ruc = new GetRequestHandler();
+            return ruc.sendGetRequestParam("https://foodrocket.herokuapp.com/api/v1/menu", params[0]);
+        }
     }
 }
