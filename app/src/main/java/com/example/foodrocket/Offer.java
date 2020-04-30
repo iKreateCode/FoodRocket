12264392b51f67d6c56624f21d39ed70a0b0a6dd
Waleed <waleedi825@gmail.com>
2020-04-30 01:04:21 +0100

    Offer & OfferItem classes created

---
+++ app/src/main/java/com/example/foodrocket/Offer.java
@@ -0,0 +1,52 @@
+package com.example.foodrocket;
+
+import java.util.ArrayList;
+
+public class Offer {
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    private ArrayList<OfferItem> offer_items;
+
+    // Getters & Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public ArrayList<OfferItem> getOffer_items() {
+        return offer_items;
+    }
+
+    public void setOffer_items(ArrayList<OfferItem> offer_items) {
+        this.offer_items = offer_items;
+    }
+}
